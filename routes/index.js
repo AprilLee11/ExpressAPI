b45c85d96450fc7ca2bf536d5e9ffa1c829cb05c
AprilLee11 <mikyung.lee11@gmail.com>
2018-11-26 20:45:13 -0500

    modify route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", function(req, res) {
-  res.redirect("/welcome");
+  res.json({ message: "It provides only API, there is not view." });
 });
 
 router.get("/welcome", function(req, res) {
-  res.json({ message: "Welcome!!" });
+  res.json({ message: "It provides only API, there is not view." });
 });
 
 module.exports = router;
